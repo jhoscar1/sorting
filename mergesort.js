b8859bae0e4f8c2dae852eba1808f44abe241b5e
Priya Murthy <prismm@gmail.com>
2017-04-27 15:48:02 -0400

    change to mergesort

--- mergesort.js
+++ mergesort.js
@@ -1,53 +1,64 @@
-function mergeSort(arr) {
-    var result = [];
-    var splitDone = function(arr) {
-        if (!arr) {
-            return false;
-        }
-        for (var i = 0; i < arr.length; i++) {
-            if (arr[i].length > 1) {
-                return false;
-            }
-        }
-        return true;
-    }
+function mergeSort(array) {
+    if (array.length < 2) return array;
+    var splits = split(array),
+        left = splits[0],
+        right = splits[1];
 
-    var noNestedArrays = function(arr) {
-        for (var i = 0; i < arr.length; i++) {
-            if (Array.isArray(arr[i])) {
-                return false;
-            }
-        }
-        return true;
-    }
-    var splitArr = [];
-    if (splitDone(arr)) {
-        console.log(splitArr);
-        result = result.concat(merge(splitArr[0], splitArr[1]));
-    }
-    else {
-        for (var i = 0; i < arr.length; i++){
-            splitArr.concat(split(arr[i]));
-        }
-    }
-    if (noNestedArrays(splitArr)){
-        return result;
-    }
-    else {
-        mergeSort(splitArr);
-    } 
+    return merge(mergeSort(left), mergeSort(right));
 }
+// var splitArr = [];
+// var result = [];
+// var splitDone = function(testArr) {
+//     if (!Array.isArray(testArr)) {
+//         return false;
+//     }
+//     for (var i = 0; i < testArr.length; i++) {
+//         if (!Array.isArray(testArr[i])) {
+//             return false;
+//         }
+//         if (testArr[i].length > 1) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// var NestedArrays = function(test) {
+//     for (var i = 0; i < test.length; i++) {
+//         if (Array.isArray(test[i])) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// //checks if the original arr still needs to be split into splitArr;
+// if (!splitDone(arr)) {
+//     for (var i = 0; i < arr.length; i++) {
+//         splitArr = splitArr.concat(split(arr));
+//         console.log(splitArr);
+//     }
+// }
+
+// //if the splitting is done, begin merging/sorting into result;
+// else result = result.concat(merge(splitArr[0], splitArr[1]));
+
+// if (!NestedArrays(splitArr)) mergeSort(splitArr);
+
+// //if merging is done, return resultl
+// else return result;
+
+//WHAT IF EXCESS ARRAY LAST VAL IS LESS THAN THE LAST VALL ON MERGED ARR?
+
 
 function split(arr) {
     var result = [];
-    var splitIndex = Math.floor(arr.length/2);
+    var splitIndex = Math.floor(arr.length / 2);
     result[0] = arr.slice(0, splitIndex);
     result[1] = arr.slice(splitIndex);
-    console.log(result);
     return result;
 }
 
-function merge(arr1, arr2){
+function merge(arr1, arr2) {
     var mergedArr = arr1.concat(arr2);
     return bubbleSort(mergedArr);
 }
